b275906ced05ab0e3a43ead2950af3ec82e391f7
Pratiyush Tiwari <aloomataloo@gmail.com>
2023-01-23 21:05:24 +0545

    Change env variable name

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ export default NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.Google_ID,
-            clientSecret: process.env.Google_Secret,
+            clientSecret: process.env.Google_SECRET,
         }),
     ],
 });
